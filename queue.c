bb949242ae9842ce19dfd30af83e3e44eb2a2661
C.W. Chien <dppss92132@gmail.com>
2022-02-26 20:51:44 +0800

    Update list API usage in q_remove_tail
    I forgot there's a list_last_entry macro.

--- queue.c
+++ queue.c
@@ -119,7 +119,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
 
     // cppcheck-suppress nullPointer
-    element_t *tail_entry = list_entry(head->prev, element_t, list);
+    element_t *tail_entry = list_last_entry(head, element_t, list);
     if (sp) {
         sp = strncpy(sp, tail_entry->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
